8dacb4ac9ce7e6fd9c81e31904c85e6411258bb7
Hana Stupková <hstupkova@gmail.com>
2021-11-06 09:08:47 +0100

    another product added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const products = [
+  {
+    id: 1,
+    title: 'Product A',
+    price: 6,
+    description: 'This is the first product - amazing!'
+  },
+  {
+    id: 2,
+    title: 'Product B',
+    price: 10,
+    description: 'This is another product - how fun!'
+  },
+]
+
 const Products = (props) => {
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {
+          products.map(item => {return <ProductItem
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />})
+        }
       </ul>
     </section>
   );
