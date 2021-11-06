ca02c0a38e9f8f595f76ac75f3bb683e639015ff
Hana Stupková <hstupkova@gmail.com>
2021-11-06 09:25:41 +0100

    number of cart items displayed

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,19 +1,20 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartActions } from '../../store/cartToggler';
 
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-
   const toggleCartHandler = () => {
     dispatch(toggleCartActions.toggleCart());
   }
 
+  const cartItems = useSelector(state => state.cartItems.items);
+
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItems.length}</span>
     </button>
   );
 };
